b486a3af2476cc87562cbd480354e8160b8b543c
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-01 15:05:38 -0700

    set width to number of characters

--- src/styles/Readouts.styled.js
+++ src/styles/Readouts.styled.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const LargeRdo = styled.p`
   font-size: x-large;
   margin: 0;
+  width: ${({ rdoLength }) => `${rdoLength}ch`};
+  text-align: right;
 `;
