9769ed272fda6bdcb62bb3d6050e2e758b6b8d71
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-01 15:06:40 -0700

    pass readout length as prop

--- src/itt/rdo/rdo.jsx
+++ src/itt/rdo/rdo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { LargeRdo } from "../../styles/Readouts.styled";
 
 const Rdo = ({ value }) => {
-  return <LargeRdo>{value}</LargeRdo>;
+  return <LargeRdo rdoLength={3}>{value}</LargeRdo>;
 };
 
 export default Rdo;
